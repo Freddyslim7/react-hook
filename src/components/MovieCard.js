4a1bf0bb78f05bb0c0b18214e610dcc2aa7a2f64
unknown <offiongebeiyamba@gmail.com>
2023-11-26 17:36:03 +0100

    Feat: code rewrite

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,12 +1,34 @@
-const MovieCard = ({ title, description, posterURL, rating }) => {
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// const handleDelete = (key) => {
+// moviesObj.splice(key, 1)
+// }
+
+// title, description, posterURL, rating
+
+const MovieCard = ({ title, description, posterUrl, rating, del }) => {
     return (
-      <li>
-        <img src={posterURL} alt={title} width={100}  />
-      <h3>{title}</h3>
-        <p>{description}</p>
-        <p>{rating}/5</p>
-      </li>
+        <>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={posterUrl} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                        {description}
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>{rating}</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                    <Card.Link href="#" onClick={del}>Delete Movie</Card.Link>
+                </Card.Body>
+            </Card>
+
+        </>
     )
-  }
-  
-  export default MovieCard
+};
+
+export default MovieCard;
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,15 +1,89 @@
-import MovieCard from './MovieCard'
+import MovieCard from "./MovieCard";
+import moviesObj from "../MoviesObj";
+import React, { useState } from "react";
+import Filter from "./Filter";
 
-const MovieList = ({ list }) => {
-  return (
-    <ul>
-      {
-        list.map((item) => (
-          <MovieCard key={item.id} {...item} />
-        ))
-      }
-    </ul>
-  )
-}
+const MovieList = () => {
+    const [movies, setMovies] = useState(moviesObj);
+    const [newMovie, setNewMovie] = useState('');
 
-export default MovieList
+    const handleDelete = (key) => {
+        const newMovies = movies.filter((_, n) => n !== key);
+        setMovies(newMovies);
+    }
+
+    const handleChange = (e) => {
+        setNewMovie({ ...newMovie, [e.target.id]: e.target.value });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMovies([...movies, newMovie]);
+    };
+
+    function handleFilter(search) {
+        let filteredMovie = movies.filter((value) =>
+            value.title.toLowerCase().includes(search.toLowerCase())
+        );
+        setMovies(filteredMovie);
+    }
+
+    return (
+        <>
+            <Filter handleFilter={handleFilter} />
+
+            <form onSubmit={handleSubmit} className="my-form">
+                <div className="input-group">
+                    <label htmlFor="title">Title</label>
+                    <input
+                        id="title"
+                        value={newMovie.title || ""}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="description">Description</label>
+                    <input
+                        id="description"
+                        value={newMovie.description || ""}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="posterUrl">PosterUrl</label>
+                    <input
+                        id="posterUrl"
+                        value={newMovie.posterUrl || ""}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="rating">Rating</label>
+                    <input
+                        id="rating"
+                        value={newMovie.rating || ""}
+                        onChange={handleChange}
+                    />
+                </div>
+                <button type="submit" className="submit-btn">
+                    Add A New Movie
+                </button>
+            </form>
+
+            <div className="cont">
+                {movies.map((movie, key) => (
+                    <MovieCard
+                        title={movie.title}
+                        description={movie.description}
+                        posterUrl={movie.posterUrl}
+                        rating={movie.rating}
+                        del={() => handleDelete(key)}
+                    />
+                ))}
+            </div>
+        </>
+
+    );
+};
+
+export default MovieList;
